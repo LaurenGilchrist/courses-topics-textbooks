efc9d9c4bee282a60d0f17a00ed6c88ca91dd228
LaurenGilchrist <laurengilchrist@hotmail.com>
2018-07-04 22:03:35 -0400

    added textbooks

---
+++ courses-topics-textbooks/src/main/java/courses/CourseRepository.java
@@ -0,0 +1,15 @@
+package courses;
+
+import java.util.Collection;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+
+	Collection<Course> findByTopicsContains(Topic topic);
+	
+	Collection<Course> findByTopicsId(Long id);
+
+	Collection<Course> findByTextbooksContains(Textbook textbook);
+
+}
